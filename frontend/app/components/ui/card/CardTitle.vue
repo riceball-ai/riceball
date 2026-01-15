3a840336bfa7d87efacf878eaa50f866d31ba7be
Rafi <rafiwx@gmail.com>
2026-01-15 17:03:10 +0800

    Update shadcn-nuxt to version 2.4.3.

--- frontend/app/components/ui/card/CardTitle.vue
+++ frontend/app/components/ui/card/CardTitle.vue
@@ -8,10 +8,10 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div
+  <h3
     data-slot="card-title"
     :class="cn('leading-none font-semibold', props.class)"
   >
     <slot />
-  </div>
+  </h3>
 </template>
